4bca052b0ada17f97efd5cd66a8beb9be3322589
phamtuan19 <phamtuan19hd@gmail.com>
2023-09-21 00:09:32 +0700

    hotfix production

--- src/@Core/Api/BaseService.ts
+++ src/@Core/Api/BaseService.ts
@@ -4,8 +4,8 @@ import createInstance from './Axios';
 
 class BaseService {
    //https://be-admin-blog-battech.vercel.app/api
-   // BASE_URL: string = 'https://be-admin-blog-battech.vercel.app/api';
-   BASE_URL: string = 'http://localhost:5001/api';
+   BASE_URL: string = 'https://be-admin-blog-battech.vercel.app/api';
+   // BASE_URL: string = 'http://localhost:5001/api';
 
    // BASE_URL: string = import.meta.env.BASE_URL;
 
